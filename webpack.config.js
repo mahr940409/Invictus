edce7f9619ffe2789a4194a72568eb20ec8347f9
Manuel Henrndez <131784503+mahr940409@users.noreply.github.com>
2025-06-05 18:57:24 -0500

    version 1.5

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './public/favicon.ico'
     }),
     new webpack.DefinePlugin(envKeys),
   ],
